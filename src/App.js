0575573655f013a610d339f791d3ad51623d95e6
HP <sreekanta.csd@gmail.com>
2022-10-01 12:59:34 +0600

    github athentication added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import app from './firebase.init'
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import { useState } from 'react';
 
 const auth = getAuth(app)
@@ -8,9 +8,11 @@ const auth = getAuth(app)
 function App() {
   const [user, setUser] = useState({})
 
-  const provider = new GoogleAuthProvider()
+  const googleProvider = new GoogleAuthProvider()
+  const githubProvider = new GithubAuthProvider()
+
  const handleGoogleSignIn= ()=>{
-  signInWithPopup(auth, provider)
+  signInWithPopup(auth, googleProvider)
   .then((result)=>{
    const user = result.user
    setUser(user)
@@ -18,6 +20,17 @@ function App() {
   })
   .catch((error)=>{
     console.log(error)
+  })
+ }
+
+ const githubSignIn= ()=>{ 
+  signInWithPopup(auth, githubProvider)
+  .then(result=>{
+    const user = result.user
+    console.log(user)
+  })
+  .catch(error=>{
+    console.error(error)
   })
  }
 
@@ -36,8 +49,11 @@ function App() {
     <div className="App">
 
      { user.email? <button onClick={handleSignOut} className='bg-red-500 p-3 m-2 rounded-lg text-white font-bold shadow-lg'>Sign Out</button>:
-     
-      <button onClick={handleGoogleSignIn} className='bg-green-500 p-3 m-2 rounded-lg text-white font-bold shadow-lg'>Google Sign In</button>
+
+ <>
+       <button onClick={handleGoogleSignIn} className='bg-green-500 p-3 m-2 rounded-lg text-white font-bold shadow-lg'>Google Sign In</button>
+      <button onClick={githubSignIn} className='bg-blue-500 p-3 m-2 rounded-lg text-white font-bold shadow-lg'>Github Sign In</button>
+ </>
     }
      <h1>Your Name: {user.displayName}</h1>
      <h1>Email: {user.email}</h1>
